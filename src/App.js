160025b5d436da14b8b31f066d2aadbf3d9fe247
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-01-30 14:26:38 -0500

    intial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Article, Brand, CTA, Feature, Navbar } from './components';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>GPT-3</h1>
+    </div>
+  );
+}
+
+export default App;
