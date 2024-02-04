ef308bb51c1095f88109eda042dd62985eddb8e3
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-02-03 23:49:00 -0500

    working on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+
+                </div>
+            </div>
         </div>
     )
 }
