6f042947f73ae646a61c871e1af0e5c9d4b81441
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-02-14 00:31:02 -0500

    CTA and possibility cleanup

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
     return (
         <div className="gpt3__cta">
             <div className="gpt3__cta-content">
-                <p>Request early access to get started</p>
+                <p>Request Early Access to get Started</p>
                 <h3>Register today & start exploring the endless possibilities.</h3>
             </div>
             <div className="gpt3__cta-button">
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,10 +9,10 @@ const Possibility = () => {
                 <img src={possibilityImage} alt="possibility" />
             </div>
             <div className="gpt3__possibility-content" >
-                <h4>Request early access to get started</h4>
+                {/* <h4>Request early access to get started</h4> */}
                 <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
-                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-                <h4>Request early access to get started</h4>
+                <p>GPT-3 opens up a world of endless possibilities in natural language processing and artificial intelligence. Its remarkable ability to understand, generate, and manipulate human-like text offers vast potential across numerous domains.</p>
+                <h4>Request Early Access to get Started</h4>
             </div>
         </div>
     )
